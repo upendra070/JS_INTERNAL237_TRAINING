e28dd5dded4b349a5650e20553e8291769d431d4
UPENDRA KUMAR DUBEY <ud9628@gmail.com>
2025-12-10 13:11:01 +0000

    DAY 3 ASSIGNMENT DONE

---
+++ .devcontainer/Assignment/Day3.js
@@ -0,0 +1,130 @@
+
+// 1-
+
+// let marks = [80, 90, 70, 85, 95];
+
+// // Calculate sum using reduce
+// let sum = marks.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+// // Calculate average
+// let average = sum / marks.length;
+
+// console.log("Average marks:", average);
+
+
+
+
+
+
+
+// 2-
+
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// // Use filter to get even numbers
+// let evenNumbers = numbers.filter(num => num % 2 === 0);
+
+// console.log("Even numbers:", evenNumbers);
+
+
+
+
+
+
+// 3-
+
+
+// let cart = {
+//   item: "Laptop",
+//   price: 45000,
+//   quantity: 2
+// };
+
+// // Calculate total bill
+// let totalBill = cart.price * cart.quantity;
+
+// console.log("Total Bill:", totalBill);
+
+
+
+
+
+ 
+
+// 4-
+
+
+// let movies = ["Inception", "Interstellar", "Avengers", "Titanic"];
+
+// // Add one movie
+// movies.push("The Matrix");
+
+// // Remove last movie
+// movies.pop();
+
+// // Print final list using loop
+// console.log("Final movie list:");
+// for(let i = 0; i < movies.length; i++) {
+//   console.log(movies[i]);
+// }
+
+
+
+
+
+
+// 5-
+// Original object
+// let user = { name: "Aman", age: 21, course: "JS" };
+
+// // Convert object to JSON string
+// let userJSON = JSON.stringify(user);
+// console.log("JSON String:", userJSON);
+
+// // Convert JSON string back to object
+// let userObj = JSON.parse(userJSON);
+// console.log("Converted back to Object:", userObj);
+
+
+
+
+
+
+
+
+// 6-
+
+// let arr = [10, 40, 25, 80, 15];
+
+// // Initialize max with the first element
+// let max = arr[0];
+
+// // Loop through the array
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i]; // Update max if current element is greater
+//   }
+// }
+
+// console.log("Maximum number:", max);
+
+
+
+
+
+
+// 7-
+
+
+// let names = ["ram", "shyam", "mohan"];
+
+// // Use map to convert each name to uppercase
+// let upperNames = names.map(name => name.toUpperCase());
+
+// console.log(upperNames);
+
+
+
+
+
