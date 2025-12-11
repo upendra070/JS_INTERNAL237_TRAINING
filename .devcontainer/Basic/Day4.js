e1374747de1ed81048b97998a765c054c9a2b7c2
UPENDRA KUMAR DUBEY <ud9628@gmail.com>
2025-12-11 05:31:02 +0000

    DAY 4 class done

---
+++ .devcontainer/Basic/Day4.js
@@ -0,0 +1,9 @@
+let newdata=new Date();
+console.log(newdata);
+let n=newdata.toDateString;
+console.log(n);
+let d=newdata.toLocaleString();
+console.log(d);
+let g=newdata.toLocaleDateString();
+console.log(g);
+
